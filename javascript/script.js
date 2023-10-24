6d57aa1832a4725b7a6331c7d9a97fbb5100ebed
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-10-24 06:47:48 +0000

    implement search, allowing user to search for specific country

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,13 @@
 let card_section = document.getElementById("card-section");
+let search_input = document.getElementById("search-input");
+
 const BASE_URL = "https://restcountries.com";
 
 // start DOM manipulation and data fetching when DOM has loaded 
 window.addEventListener("load", async ()=>{
-  let all_countries = await getData(BASE_URL+"/v3.1/all");
-  displayData(all_countries);
+  let all_countries = await getCountry(BASE_URL+"/v3.1/all");
+  displayCountry(all_countries);
+  searchCountry(search_input);
 })
 
 
@@ -13,8 +16,8 @@ window.addEventListener("load", async ()=>{
 
 ///////////////////////////////////////////////
 
-// fetch data from API 
-async function getData(url){
+// fetch data from API and return it
+async function getCountry(url){
     try{
       let response = await fetch(url);
 
@@ -33,19 +36,27 @@ async function getData(url){
 };
 
 
-//display data to the DOM 
-function displayData(data){
-  // loop through data and create new cards with countries details
+//accept a an array of countries and create bootsrap cards displying them to the DOM 
+function displayCountry(data){
+  // Delete existing cards from the DOM if there's any 
+  while (card_section.firstChild) {
+    card_section.removeChild(card_section.firstChild);
+  }
+
+  // create new cards from fetched countries
   data.forEach(ele => {
     // HTML card template 
     let card = `
     <div class="card shadow-sm border-0 mb-5" style="width: 18rem">
         <div class="h-75">
-          <img
+          <a href="">
+            <img
             class="card-img-top w-100 h-100" 
             src="${ele.flags.png?ele.flags.png:"./images/download.svg"}"
             alt="Card image cap"
-          />
+            />
+          </a>
+          
         </div>
         
         <div class="card-body py-4">
@@ -58,9 +69,19 @@ function displayData(data){
         </div>
     </div>`;
 
-    //add new card to DOM based on the number of countries 
+    //add new cards to DOM 
     card_section.insertAdjacentHTML("beforeend", card);
   });
+}
+
+//accept an html input field, and diplay countries that match user input
+function searchCountry(html_text_input){
+  html_text_input.addEventListener("input", async ()=>{
+    let country_to_search = search_input.value;
+    let country = await getCountry(`https://restcountries.com/v3.1/name/${country_to_search}`);
+   
+    displayCountry(country);
+  })
 }
        
 
