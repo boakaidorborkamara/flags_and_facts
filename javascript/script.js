af81a4c4024c2dfcf297127abe60411cbcf39f21
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-10-26 04:56:37 +0000

    implement toggling between light mode and dark mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,10 @@
+let body = document.getElementsByTagName("body");
+let header = document.getElementsByTagName("header");
 let card_section = document.getElementById("card-section");
 let search_input = document.getElementById("search-input");
 let filter_dropdown = document.getElementById("filter-dropdown");
+let theme_btn = document.getElementById("theme-btn");
+let theme_bnt_text = document.getElementById("theme-btn-text")
 const BASE_URL = "https://restcountries.com";
 
 
@@ -10,7 +14,8 @@ window.addEventListener("load", async ()=>{
   let all_countries = await getCountry(BASE_URL+"/v3.1/all");
   displayCountry(all_countries);
   searchCountry(search_input);
-  filterCountry(filter_dropdown);
+  filterCountriesByRegion(filter_dropdown);
+  toggleTheme(theme_btn);
 })
 
 
@@ -35,7 +40,6 @@ async function getCountry(url){
       }
     }
 };
-
 
 //accept a an array of countries and create bootsrap cards displying them to the DOM 
 function displayCountry(data){
@@ -94,6 +98,53 @@ function filterCountriesByRegion(html_dropdown_list){
      let country = await getCountry(`https://restcountries.com/v3.1/region/${selected_region}`);
      displayCountry(country);
   })
+}
+
+//change the UI of the application based on the selected theme (dark or light)
+function toggleTheme(btn){
+
+  btn.addEventListener("click", ()=>{
+    
+    //the value of theme btn indicating user selected text
+    let user_selected_theme = theme_bnt_text.innerText;
+    
+    //toggle between light and dark mode
+    if(user_selected_theme==="Dark Mode"){
+      // remove light mode styling from elements
+      body[0].classList.remove("body-background-light");
+      header[0].classList.remove("bg-light");
+      header[0].classList.remove("text-dark");
+      theme_btn.classList.remove("text-dark");
+
+      // and change styling to match dark mode 
+      body[0].classList.add("body-background-dark");
+      header[0].classList.add("bg-dark");
+      header[0].classList.add("text-light");
+      theme_btn.classList.add("text-light");
+
+      // change innerText of the theme button 
+      theme_bnt_text.innerText = "Light Mode";
+
+    }
+    else if(user_selected_theme==="Light Mode"){
+      // remove dark mode styling from elements 
+      body[0].classList.remove("body-background-dark");
+      header[0].classList.remove("bg-dark");
+      header[0].classList.remove("text-light");
+      theme_btn.classList.remove("text-light");
+
+      // and add styling to match light mode 
+      body[0].classList.add("body-background-light");
+      header[0].classList.add("text-dark");
+      theme_btn.classList.add("text-dark");
+
+      // change inner text of the theme value 
+      theme_bnt_text.innerText = "Dark Mode";
+      
+    }
+
+  })
+
 }
        
 
